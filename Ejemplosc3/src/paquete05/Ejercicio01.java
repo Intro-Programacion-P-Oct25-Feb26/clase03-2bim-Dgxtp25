eecc3de1c9bafb2607cc2ac1619ebf5b7efb26aa
Diego B. <dgxtpbr@gmail.com>
2025-12-17 12:58:41 -0500

    Cambio1 - Ejercicio 5

--- Ejemplosc3/src/paquete05/Ejercicio01.java
+++ Ejemplosc3/src/paquete05/Ejercicio01.java
@@ -3,11 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package paquete05;
-
+import java.util.Scanner;
 /**
  *
  * @author dgxbt
  */
 public class Ejercicio01 {
+    public static void main(String[] args){
+        Scanner entrada = new
+    Scanner (System.in);
+        String[] nombresEstacion = new String[5];
+        String[] nombresEncargados = new String[5];
+        double[][] produccion = new double [5][12];
+        double [] totalProduccion = new double [5];
+        for (int i = 0; i < 5; i++){
+            System.out.println("Ingrese nombre de la estacion" + (i + 1) + ":");
+            nombresEstacion[i] = entrada.nextLine();
+            System.out.println("Ingrese nombre del encargado de la estacion" 
+                    + (i + 1) + ":");
+            nombresEncargados[i] = entrada.nextLine();
+          for (int j = 0; j < 12; j++){
+              System.out.println("Produccion del mes" + (j + 1) + ":");
+            produccion[i][j] = entrada.nextDouble();
+            totalProduccion[i]+= entrada.nextDouble();
+              
+          }
+            
+        }
+        
+    }
     
 }
